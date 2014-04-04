0877a7c7d5cfed324e2f184e6a77cf42d0b82ea7
Burin Asavesna <burinasavesna@gmail.com>
2014-04-04 10:04:50 -0500

    remove unused h5bp stuff, add buttons to insert dom elements on the page, add separate "load timing"

--- js/node_insertion_detection.js
+++ js/node_insertion_detection.js
@@ -1,5 +1,5 @@
 // Inject the css that sets up the animations
-var css = '@keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-moz-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-webkit-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-ms-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-o-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}[data-timing]{-webkit-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-ms-animation-name:nodeInserted;-o-animation-name:nodeInserted;animation-name:nodeInserted;-webkit-animation-duration:.001s;-moz-animation-duration:.001s;-ms-animation-duration:.001s;-o-animation-duration:.001s;animation-duration:.001s}';
+var css = '@keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-moz-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-webkit-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-ms-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-o-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}[data-timing],[data-load-timing]{-webkit-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-ms-animation-name:nodeInserted;-o-animation-name:nodeInserted;animation-name:nodeInserted;-webkit-animation-duration:.001s;-moz-animation-duration:.001s;-ms-animation-duration:.001s;-o-animation-duration:.001s;animation-duration:.001s}';
 var head = document.head || document.getElementsByTagName('head')[0];
 var style = document.createElement('style');
 
@@ -17,9 +17,12 @@ head.appendChild(style);
 var timingNodeInserted = function(event){
     if (event.animationName === 'nodeInserted') {
         var timingName = event.target.getAttribute('data-timing');
+        var loadTimingName = event.target.getAttribute('data-load-timing');
         if (timingName) {
             console.timeEnd(timingName);
-            console.log('Time since load: ', window.performance.now());
+        }
+        if (loadTimingName) {
+            console.log('Time since load: ', loadTimingName, window.performance.now());
         }
     }
 };
