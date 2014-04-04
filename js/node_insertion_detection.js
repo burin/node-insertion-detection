56f21a8f5b2837d49faa91fd9234f9feaaa12199
Burin Asavesna <burinasavesna@gmail.com>
2014-04-04 10:20:57 -0500

    wrap in iife

--- js/node_insertion_detection.js
+++ js/node_insertion_detection.js
@@ -1,3 +1,4 @@
+(function(){
 // Inject the css that sets up the animations
 var css = '@keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-moz-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-webkit-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-ms-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-o-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}[data-timing],[data-load-timing]{-webkit-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-ms-animation-name:nodeInserted;-o-animation-name:nodeInserted;animation-name:nodeInserted;-webkit-animation-duration:.001s;-moz-animation-duration:.001s;-ms-animation-duration:.001s;-o-animation-duration:.001s;animation-duration:.001s}';
 var head = document.head || document.getElementsByTagName('head')[0];
@@ -30,3 +31,4 @@ var timingNodeInserted = function(event){
 document.addEventListener('animationstart', timingNodeInserted, false);
 document.addEventListener('MSAnimationStart', timingNodeInserted, false);
 document.addEventListener('webkitAnimationStart', timingNodeInserted, false);
+})();
